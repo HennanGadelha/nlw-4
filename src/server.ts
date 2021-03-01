17dd05e630b9101fc5254f10e7adf5f2e196b7a8
Hennan Gadelha <hennanhennan@gmail.com>
2021-03-01 17:12:18 -0300

    inciando projeto

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import 'reflect-metadata';
+import express from 'express';
+import "./database";
+import { router } from './routes';
+
+const app = express();
+
+app.use(express.json())
+
+app.get('/', (request, response) =>{
+
+    return response.json({message: "hello json"})
+
+})
+
+
+app.use(router)
+
+
+
+app.listen(3333, () =>{
+    console.log("Servidor rodando");
+})
